8ca01a2332c6fa10cd7c4eecc40b7cc2445e3b89
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-19 13:09:08 +0100

    not working app with 406 error

---
+++ src/main/java/pl/javastart/controller/CityController.java
@@ -0,0 +1,45 @@
+package pl.javastart.controller;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import pl.javastart.model.City;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+@EnableWebMvc
+public class CityController {
+
+    private List<City> cities;
+
+    public CityController() {
+        cities = Collections.synchronizedList(new ArrayList<City>());
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<City> getCities(@RequestParam(defaultValue = "name") String orderBy) {
+        List<City> citiesCopy = new ArrayList<>(cities);
+        if ("name".equals(orderBy)) {
+            citiesCopy.sort(Comparator.comparing(City::getName));
+        } else if ("population".equals(orderBy)) {
+            citiesCopy.sort(Comparator.comparing(City::getPopulation));
+        }
+        return citiesCopy;
+    }
+
+    @GetMapping("/{id}")
+    public City getCity(@PathVariable int id){
+        return cities.get(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveCity(@RequestBody City city){
+        cities.add(city);
+    }
+}
